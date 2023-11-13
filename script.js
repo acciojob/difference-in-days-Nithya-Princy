0bd04f4ce7468113dc743f8c5e2f6c5359b2537b
Nithya-Princy <141929247+Nithya-Princy@users.noreply.github.com>
2023-11-13 20:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  const firstDate = new Date(date1);
+  const secondDate = new Date(date2);
+
+  // Calculate the time difference in milliseconds
+  const timeDifference = secondDate - firstDate;
+
+  // Calculate the number of days by dividing the time difference by the number of milliseconds in a day
+  const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  return daysDifference;
 };
 
 // Do not change the code below.
